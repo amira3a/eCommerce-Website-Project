f6fbb75cf8c330963ca264dd21d28a3a6666383a
amira3a <amira3a2780@yahoo.com>
2024-09-11 12:13:49 +0300

    fixing contact form

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -24,36 +24,43 @@ export default function Contact(props) {
     });
   };
 
-  const validate = () => {
-    const newErrors = {};
+  // const validate = () => {
+  //   const newErrors = {};
 
-    if (!formData.name.trim()) {
-      newErrors.name = "Name is required";
-    }
+  //   // if (!formData.name.trim()) {
+  //   //   newErrors.name = "Name is required";
+  //   // }
 
-    if (!formData.email.trim()) {
-      newErrors.email = "Email is required";
-    } else if (!/\S+@\S+\.\S+/.test(formData.email)) {
-      newErrors.email = "Email is invalid";
-    }
+  //   // if (!formData.email.trim()) {
+  //   //   newErrors.email = "Email is required";
+  //   // } else if (!/\S+@\S+\.\S+/.test(formData.email)) {
+  //   //   newErrors.email = "Email is invalid";
+  //   // }
 
-    if (!formData.message.trim()) {
-      newErrors.message = "Message is required";
-    }
+  //   // if (!formData.message.trim()) {
+  //   //   newErrors.message = "Message is required";
+  //   // }
 
-    setErrors(newErrors);
-    return Object.keys(newErrors).length === 0;
-  };
+  //   setErrors(newErrors);
+  //   return Object.keys(newErrors).length === 0;
+  // };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (validate()) {
-      alert("Subscribed successfully!");
+    // if (validate()) {
+      alert("The message was sent successfully. Thank you!");
       console.log("Form data submitted:", formData);
-    } else {
-      console.log("Form validation failed");
-    }
-  };
+    //}
+    // else {
+    //   console.log("Form validation failed");
+    // }
+    setFormData({
+      name: "",
+      email: "",
+      subject: "",
+      message: "",
+    })
+  }
 
   return (
     <>
